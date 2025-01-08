e13dae1d4722e2dbe5a3657a552dad4c038bc635
Anuar Zhangeldi <anuar1202@gmail.com>
2025-01-08 16:07:14 +0500

    Edited configs and tests

--- src/test/java/tests/EmulationOnboardingTests.java
+++ src/test/java/tests/EmulationOnboardingTests.java
@@ -4,21 +4,22 @@ import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import screens.ErrorScreen;
 import screens.SearchScreen;
 import screens.components.IntroComponent;
 
 @Tag("emulation")
 @Owner("Anuar Zhangeldi")
-@DisplayName("Tests for Wikipedia Onboarding Screens")
+@DisplayName("Tests for Wikipedia Onboarding Screens and Search via Emulator")
 
-public class EmulationOnboardingTests extends TestBase{
+public class EmulationOnboardingTests extends TestBase {
     public final IntroComponent introComponent = new IntroComponent();
 
     public final SearchScreen searchScreen = new SearchScreen();
 
     @DisplayName("Successful verification of Onboarding Screens")
     @Test
-    public void SuccessfulOnboardingTest(){
+    public void SuccessfulOnboardingTest() {
         introComponent.verifyTitleOnTheScreen("The Free Encyclopedia")
                 .clickOnContinueButton()
                 .verifyTitleOnTheScreen("New ways to explore")
@@ -29,5 +30,28 @@ public class EmulationOnboardingTests extends TestBase{
                 .clickOnGetStarted();
         searchScreen.checkAnnoncementText("Customize your Explore feed");
 
+    }
+
+    @Test
+    @DisplayName("Verification of the successful search result displaying for Appium")
+    void successfulSearchTest() {
+        introComponent.clickOnContinueButton()
+                .clickOnContinueButton()
+                .clickOnContinueButton()
+                .clickOnGetStarted();
+        SearchScreen.performSearch("Android");
+        SearchScreen.verifySearchResultsPresent();
+    }
+
+    @Test
+    @DisplayName("Testing of the error message displaying for invalid search")
+    void invalidSerchExecutionTest() {
+        introComponent.clickOnContinueButton()
+                .clickOnContinueButton()
+                .clickOnContinueButton()
+                .clickOnGetStarted();
+        SearchScreen.performSearch("TTT");
+        SearchScreen.openFirstSearchResult();
+        ErrorScreen.verifyErrorMessage();
     }
 }
