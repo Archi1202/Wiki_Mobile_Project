04fabe5923f710741fcc54be17eee4431f9268b3
Anuar Zhangeldi <anuar1202@gmail.com>
2025-01-27 21:07:34 +0500

    edit driver

--- src/test/java/drivers/BrowserStackDriver.java
+++ src/test/java/drivers/BrowserStackDriver.java
@@ -4,11 +4,12 @@ import com.codeborne.selenide.WebDriverProvider;
 import configs.AuthConfig;
 import configs.WebDriverConfig;
 import helpers.Browserstack;
-import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
@@ -16,29 +17,50 @@ import java.net.URL;
 
 public class BrowserStackDriver implements WebDriverProvider {
 
-    WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
-    AuthConfig authConfig =ConfigFactory.create(AuthConfig.class,System.getProperties());
+    private static final Logger logger = LoggerFactory.getLogger(BrowserStackDriver.class);
 
-    public Browserstack browserstack = new Browserstack();
+    private final WebDriverConfig config;
+    private final AuthConfig authConfig;
+    private final Browserstack browserstack;
 
+    public BrowserStackDriver(WebDriverConfig config, AuthConfig authConfig, Browserstack browserstack) {
+        this.config = config;
+        this.authConfig = authConfig;
+        this.browserstack = browserstack;
+    }
 
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        logger.info("Setting up capabilities for BrowserStack driver...");
+
+        MutableCapabilities caps = setupCapabilities();
+
+        try {
+            logger.info("Creating RemoteWebDriver with URL: {}", config.getRemoteUrl());
+            return new RemoteWebDriver(new URL(config.getRemoteUrl()), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Failed to create RemoteWebDriver with URL: " + config.getRemoteUrl(), e);
+        }
+    }
+
+    private MutableCapabilities setupCapabilities() {
         MutableCapabilities caps = new MutableCapabilities();
 
-        caps.setCapability("browserstack.user", authConfig.getBrowserstackUser());
-        caps.setCapability("browserstack.key", authConfig.getBrowserstackKey());
+        // Validate and log critical configurations
+        String user = authConfig.getBrowserstackUser();
+        String key = authConfig.getBrowserstackKey();
+        if (user == null || user.isEmpty() || key == null || key.isEmpty()) {
+            throw new IllegalArgumentException("BrowserStack credentials are not properly configured.");
+        }
 
+        caps.setCapability("browserstack.user", user);
+        caps.setCapability("browserstack.key", key);
         caps.setCapability("app", browserstack.checkUploadedAppsList());
-
         caps.setCapability("device", config.getDevice());
         caps.setCapability("os_version", config.getOsVersion());
-        try {
-            return new RemoteWebDriver(
-                    new URL(config.getRemoteUrl()), caps);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+
+        logger.info("Capabilities configured: Device={}, OS version={}", config.getDevice(), config.getOsVersion());
+        return caps;
     }
 }
