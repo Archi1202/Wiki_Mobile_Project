a4df9a096aecfb9a9362aa489e1b72ea8452242e
Anuar Zhangeldi <anuar1202@gmail.com>
2025-01-28 13:53:04 +0500

    edit driver

--- src/test/java/drivers/BrowserStackDriver.java
+++ src/test/java/drivers/BrowserStackDriver.java
@@ -9,16 +9,12 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BrowserStackDriver implements WebDriverProvider {
-
-    private static final Logger logger = LoggerFactory.getLogger(BrowserStackDriver.class);
 
     private final WebDriverConfig config = ConfigReader.INSTANCE.readWebDriverConfig();
     private final AuthConfig authConfig = ConfigReader.INSTANCE.readAuthConfig();
@@ -27,19 +23,6 @@ public class BrowserStackDriver implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-        logger.info("Setting up capabilities for BrowserStack driver...");
-
-        MutableCapabilities caps = setupCapabilities();
-
-        try {
-            logger.info("Creating RemoteWebDriver with URL: {}", config.getRemoteUrl());
-            return new RemoteWebDriver(new URL(config.getRemoteUrl()), caps);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Failed to create RemoteWebDriver with URL: " + config.getRemoteUrl(), e);
-        }
-    }
-
-    private MutableCapabilities setupCapabilities() {
         MutableCapabilities caps = new MutableCapabilities();
 
         caps.setCapability("browserstack.user", authConfig.getBrowserstackUser());
@@ -48,7 +31,11 @@ public class BrowserStackDriver implements WebDriverProvider {
         caps.setCapability("device", config.getDevice());
         caps.setCapability("os_version", config.getOsVersion());
 
-        logger.info("Capabilities configured: Device={}, OS version={}", config.getDevice(), config.getOsVersion());
-        return caps;
+        try {
+            return new RemoteWebDriver(
+                    new URL(config.getRemoteUrl()), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
